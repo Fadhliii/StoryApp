4675215d8a902f714e3e2f6116549df4f2fda3fe
Fadhli M <fadhli2316@gmail.com>
2024-05-31 08:38:05 +0700

    6th Commit || Update Apiconfig with token

--- app/src/main/java/com/example/gagalmuluyaallah/ApiConfig.kt
+++ app/src/main/java/com/example/gagalmuluyaallah/ApiConfig.kt
@@ -9,26 +9,31 @@ import retrofit2.create
 import java.util.concurrent.TimeUnit
 
 object ApiConfig {
-    private val okhttp = OkHttpClient.Builder()
-        .apply {
-            // logging interceptor is for logging the request and response of the API in the logcat
-            val loggingInterceptor = HttpLoggingInterceptor()
-            loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
-            addInterceptor(loggingInterceptor)
-        }
-        .readTimeout(25, TimeUnit.SECONDS) // 25 seconds
-        .writeTimeout(300, TimeUnit.SECONDS)
-        .connectTimeout(60, TimeUnit.SECONDS)
-        .build()
+    fun getApiService(token: String): ApiService {
+        val okhttp = OkHttpClient.Builder()
+            .apply {
+                val loggingInterceptor = HttpLoggingInterceptor()
+                loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+                addInterceptor(loggingInterceptor)
 
-    // retrofit is for the retrofit builder to build the retrofit object to access the API endpoints
-    val retrofit = Retrofit.Builder()
-        .baseUrl("https://story-api.dicoding.dev/v1")
-        .client(okhttp) // client is for the okhttp
-        .addConverterFactory(
-                GsonConverterFactory.create()) // converter is for the gson converter factory
-        // to convert the response of the API to the data class that we have created in the model
-        .build()
+                addInterceptor { chain ->
+                    val request = chain.request().newBuilder()
+                        .addHeader("Authorization", "Bearer $token")
+                        .build()
+                    chain.proceed(request)
+                }
+            }
+            .readTimeout(25, TimeUnit.SECONDS)
+            .writeTimeout(300, TimeUnit.SECONDS)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .build()
 
-    val getApiService = retrofit.create<ApiService>() // lebih singkat dan bersih generic
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://story-api.dicoding.dev/v1")
+            .client(okhttp)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        return retrofit.create<ApiService>() // lebih singkat dan bersih generic
+    }
 }
